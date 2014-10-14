164446fcd90326d3dfadf7477e7b90bea7aa2f73
Mark Janssen <mark@eveoh.nl>
2014-10-14 11:28:40 +0200

    Re-add JavaPlugin import, fixes #25

--- src/main/groovy/aspectj/AspectJPlugin.groovy
+++ src/main/groovy/aspectj/AspectJPlugin.groovy
@@ -6,6 +6,7 @@ import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.file.FileCollection
 import org.gradle.api.logging.LogLevel
+import org.gradle.api.plugins.JavaPlugin
 import org.gradle.api.tasks.SourceSet
 import org.gradle.api.tasks.TaskAction
 
